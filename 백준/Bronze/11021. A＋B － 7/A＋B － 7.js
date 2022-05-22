f0e2401aa34dda255b93bb6ab4ab57362aa92cf8
Lee Young Woo <60170829+Everylisy@users.noreply.github.com>
2022-05-22 22:54:25 +0900

    [Bronze III] Title: A+B - 7, Time: 116 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,14 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let result = "";
+
+for (let i = 1; i <= input[0]; i++) {
+  let nums = input[i].split(" ");
+  result += `Case #${i}: ` + (parseInt(nums[0]) + parseInt(nums[1]) + "\n");
+}
+
+console.log(result);
