5ed4f71c8a28e55611f491a4ef7de48d920d31fa
Lee Young Woo <60170829+Everylisy@users.noreply.github.com>
2022-05-22 23:25:17 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 116 ms, Memory: 9504 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,15 @@
+const input = +require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim();
+
+let star = "";
+
+for (let i = 0; i <= input; i++) {
+  for (let j = 0; j < i; j++) {
+    star += "*";
+  }
+  star += "\n";
+}
+
+console.log(star.trim());
