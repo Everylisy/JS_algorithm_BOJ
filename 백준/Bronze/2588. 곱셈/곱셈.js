0829894df42d87a6846e38e7b3139e8672097c9a
Lee Young Woo <60170829+Everylisy@users.noreply.github.com>
2022-07-10 22:28:43 +0900

    [Bronze III] Title: 곱셈, Time: 116 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -2,14 +2,9 @@ let input = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .split("\n");
-const topNum = parseInt(input[0]);
-const btmNum = parseInt(input[1]);
-
-const oneNum = btmNum % 10;
-const tenNum = Math.floor((btmNum % 100) / 10);
-const hunNum = Math.floor((btmNum % 1000) / 100);
-
-console.log(topNum * oneNum);
-console.log(topNum * tenNum);
-console.log(topNum * hunNum);
+let topNum = input[0];
+let btmNum = input[1];
+console.log(topNum * btmNum[2]);
+console.log(topNum * btmNum[1]);
+console.log(topNum * btmNum[0]);
 console.log(topNum * btmNum);
