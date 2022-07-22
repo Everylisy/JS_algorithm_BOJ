2a6a66f80b6c8c0ed65efa6fb3f258dbcebced03
Lee Young Woo <60170829+Everylisy@users.noreply.github.com>
2022-07-22 22:58:28 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 9680 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.js
@@ -0,0 +1,20 @@
+const input = require("fs")
+  .readFileSync(__dirname + "dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const score = input[1].split(" ").map((num) => Number(num));
+const maxScore = Math.max(...score);
+let newScore = [];
+
+for (let i = 0; i < score.length; i++) {
+  newScore.push((score[i] / maxScore) * 100);
+}
+
+const result = newScore.reduce(function add(sum, currValue) {
+  return sum + currValue;
+}, 0);
+
+const average = result / newScore.length;
+console.log(average);
